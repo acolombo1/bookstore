08421d6cddd235f8930eff74281d371d0fbf3292
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-06-07 11:47:16 -0300

    Fix books to hardcoded list

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import store from '../redux/configureStore';
+// import store from '../redux/configureStore';
 import './App.css';
 import Header from './Header';
 import Book from './Book';
 import MyAddBook from './AddBook';
-import { removeBookObject } from '../redux/books/books';
+// import { removeBookObject } from '../redux/books/books';
 
 export default function App() {
-  const { books } = store.getState();
-  const remBook = (id) => {
-    store.dispatch(removeBookObject(id));
-  };
+  // const { books } = store.getState();
+  // const remBook = (id) => {
+  //   store.dispatch(removeBookObject(id));
+  // };
+
+  // {books.map((book) => <Book key={book.id} category="??"
+  // title={book.title} author={book.author} onClick={() => remBook(book.id)} />)}
 
   return (
     <div className="App">
       <Header />
-      {books.map((book) => <Book key={book.id} category="??" title={book.title} author={book.author} onClick={() => remBook(book.id)} />)}
+      <Book category="Action" title="The Hunger Game" author="Suzanne Collins" />
+      <Book category="Science Fiction" title="Dune" author="Frank Herbert" />
+      <Book category="Economy" title="Capital in the 21st Century" author="Suzanne Collins" />
       <MyAddBook />
     </div>
   );
